9dce745c746ce1798c1b0c91e034384fa3e50f45
Adam Weissman <adam@comedytragedyepic.com>
2019-09-16 09:39:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,11 @@
 def my_collect(an_array)
-  if an_array.length == 0
-    return 0
-  else
     i = 0
     collection = []
       while i < an_array.length
       collection << yield(an_array[i])
       i += 1
       end
-    return collection
-  end
+    collection
 end
 
 
